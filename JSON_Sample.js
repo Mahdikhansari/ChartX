af365e992f08193c98064e74b063bac93b6b30c4
Mahdi Khansari <mahdi.khansari@gmail.com>
2023-04-25 22:44:59 -0600

    Add files via upload

---
+++ JSON_Sample.js
@@ -0,0 +1,202 @@
+var json_data =
+{
+    "title":                "Network Level VoNR Accessibility",
+    "subTitle":             "HOU, CLE, JUS",
+    "legend":               false,
+    "x":
+    {
+        "data_type":        "dateTime",
+        "label":            "Time",
+        "unit":             "s",
+        "values":           
+            [0,1,2,3,4,5,6,7,8,9,
+            10,11,12,13,14,15,16,
+            17,18,19]  
+    },
+
+    "y" : 
+    [
+        {
+            "id":           "y_0",
+            "label":        "KPI_1",
+            "unit":         "%",      
+            "chart_type":   "line",
+            "color":        "red",
+            "dots":         true,
+            "dots-radius":  "3",
+            "stroke-width": "2",
+            "secondary_axis":false,
+            "values":       
+                [10,3,4,2,1,8,3,3,9,
+                10,3,4,2,1,8,3,3,9,2,3]
+        },
+        {
+            "id":           "y_1",
+            "label":        "Mahdi",
+            "unit":         "Mbps",      
+            "chart_type":   "line",
+            "color":        "gray",
+            "dots":         false,
+            "stroke-width": "2",
+            "secondary_axis":false,
+            "values":       
+                [2,4,5,3,2,1,2,44,20,
+                2,4,5,3,2,1,2,44,20,19,15]
+        },
+        {
+            "id":           "y_1",
+            "label":        "Arash",
+            "unit":         "%",      
+            "chart_type":   "line",
+            "color":        "blue",
+            "dots":         true,
+            "stroke-width": "3",
+            "secondary_axis":false,
+            "values":       
+                [0,2,3,1,0,7,2,2,8,
+                0,2,3,1,0,7,2,2,8,4,2]
+        },
+        {
+            "id":           "y_1",
+            "label":        "New_KPI",
+            "unit":         "Kg",      
+            "chart_type":   "line",
+            "color":        "Green",
+            "dots":         false,
+            "stroke-width": "2",
+            "secondary_axis":true,
+            "values":       
+                [10,12,13,21,10,27,32,22,18,
+                20,2,33,21,10,7,21,2,18,24,12]
+        }
+    ]
+}
+;
+
+json_data_3 =[
+    {
+        "__time": "2023-04-24T00:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 98.81
+    },
+    {
+        "__time": "2023-04-24T01:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 100
+    },
+    {
+        "__time": "2023-04-24T02:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 98.9
+    },
+    {
+        "__time": "2023-04-24T03:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 0
+    },
+    {
+        "__time": "2023-04-24T04:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 75
+    },
+    {
+        "__time": "2023-04-24T05:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 0
+    },
+    {
+        "__time": "2023-04-24T06:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 0
+    },
+    {
+        "__time": "2023-04-24T07:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 0
+    },
+    {
+        "__time": "2023-04-24T08:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 0
+    },
+    {
+        "__time": "2023-04-24T09:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 0
+    },
+    {
+        "__time": "2023-04-24T10:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 0
+    },
+    {
+        "__time": "2023-04-24T11:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 0
+    },
+    {
+        "__time": "2023-04-24T12:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 99.34
+    },
+    {
+        "__time": "2023-04-24T13:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 0
+    },
+    {
+        "__time": "2023-04-24T14:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 100
+    },
+    {
+        "__time": "2023-04-24T15:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 100
+    },
+    {
+        "__time": "2023-04-24T16:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 100
+    },
+    {
+        "__time": "2023-04-24T17:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 100
+    },
+    {
+        "__time": "2023-04-24T18:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 100
+    },
+    {
+        "__time": "2023-04-24T19:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 120
+    },
+    {
+        "__time": "2023-04-24T20:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 99.33
+    },
+    {
+        "__time": "2023-04-24T21:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 98.74
+    },
+    {
+        "__time": "2023-04-24T22:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 95.35
+    },
+    {
+        "__time": "2023-04-24T23:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 100
+    },
+    {
+        "__time": "2023-04-25T00:00:00.000Z",
+        "cell": "HOHOU00121B_3_n71_G",
+        "vonr_access": 0
+    }
+];
---
+++ JSON_Sample2.js
@@ -0,0 +1,43 @@
+var json_data_2 =
+{
+    "title":                "Network Level VoNR Accessibility",
+    "subTitle":             "HOU, CLE, JUS",
+    "legend":               false,
+    "x":
+    {
+        "data_type":        "ZuluTime",
+        "label":            "Time",
+        "unit":             "s",
+        "values":           
+            // [0,1,2,3,4,5,6,7,8,9,
+            // 10,11,12,13,14,15,16,
+            // 17,18,19]  
+            ['2023-01-01T01:00:00.000T',
+            '2023-01-01T02:00:00.000T',
+            '2023-01-01T03:00:00.000T',
+            '2023-01-01T04:00:00.000T',
+            '2023-01-01T05:00:00.000T',
+            '2023-01-01T06:00:00.000T',
+            '2023-01-01T07:00:00.000T',
+            '2023-01-01T08:00:00.000T',
+            '2023-01-01T09:00:00.000T',
+            '2023-01-01T10:00:00.000T']
+    },
+    "y" : 
+    [
+        {
+            "id":           "y_0",
+            "label":        "KPI_1",
+            "unit":         "%",      
+            "chart_type":   "line",
+            "color":        "red",
+            "dots":         true,
+            "dots-radius":  "3",
+            "stroke-width": "2",
+            "secondary_axis":false,
+            "values":       
+                [8,7,4,5,6,3,2,6,4,2]
+        }
+    ]
+}
+;
